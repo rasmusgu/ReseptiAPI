2d9c7a8155d0fb4e03919e59f2bea4cf12efabee
Rasmus Gustafsson <rasmus.gustafsson@mail.com>
2019-03-05 18:37:26 +0200

    Joku tuli

--- mysqlConnect.js
+++ mysqlConnect.js
@@ -1,21 +1,22 @@
 var mariadb = require('mariadb')
 
-mariadb.createConnection({ // Open a new connection
-    user: 'monty',
-    password: 'metrofilia1',
-    host: 'haxers.ddns.net',
-    port: 3306
-})
-    .then(conn => {
-        conn.query('SELECT "Hello world!" as my_message') // Execute a query
-            .then(result => { // Print the results
-                for (row of result) {
-                    console.log(row)
-                }
-            })
-            .then(conn.destroy()) // Close the connection
+function mysqlConnectionTest() {
+    mariadb.createConnection({ // Open a new connection
+        user: 'monty',
+        password: 'metrofilia1',
+        host: 'haxers.ddns.net',
+        port: 3306
     })
-
+        .then(conn => {
+            conn.query('SELECT "Hello world!" as my_message') // Execute a query
+                .then(result => { // Print the results
+                    for (row of result) {
+                        console.log(row)
+                    }
+                })
+                .then(conn.destroy()) // Close the connection
+        })
+}
 
 function reseptiLista() {
     mariadb.createConnection({ // Open a new connection
@@ -47,7 +48,7 @@ function reseptiHaku(reseptinNimi) {
     })
         .then(conn => {
             conn.query('use reseptiapi') // Execute a query
-            conn.query('SELECT * FROM reseptit') // Execute a query
+            conn.query('SELECT "' + reseptinNimi + '" FROM reseptit') // Execute a query
                 .then(result => { // Print the results
                     for (row of result) {
                         console.log(row)
@@ -58,3 +59,60 @@ function reseptiHaku(reseptinNimi) {
         })
 
 }
+
+function ainesosaHaku(reseptiId) {
+    mariadb.createConnection({ // Open a new connection
+        user: 'monty',
+        password: 'metrofilia1',
+        host: 'haxers.ddns.net',
+        port: 3306
+    })
+        .then(conn => {
+            conn.query('use reseptiapi') // Execute a query
+            conn.query('SELECT nimi FROM ainesosat WHERE reseptiID=reseptiId') // Execute a query
+                .then(result => { // Print the resultsr
+                    for (row of result) {
+                        console.log(row)
+                    }
+                })
+                .then(conn.destroy()) // Close the connection
+            return result
+        })
+
+}
+
+function syotaResepti(nimi, valmistusaika, kokkausohje) {
+    mariadb.createConnection({ // Open a new connection
+        user: 'monty',
+        password: 'metrofilia1',
+        host: 'haxers.ddns.net',
+        port: 3306
+    })
+        .then(conn => {
+            conn.query('use reseptiapi') // Execute a query
+            conn.query('INSERT INTO reseptit(nimi, valmistusaika, kokkausohje) VALUES("'+nimi+'", '+valmistusaika+', "'+kokkausohje+'")') // Execute a query
+                .then(conn.destroy()) // Close the connection
+        })
+}
+
+function syotaAinesosa(nimi, reseptiId) {
+    mariadb.createConnection({ // Open a new connection
+        user: 'monty',
+        password: 'metrofilia1',
+        host: 'haxers.ddns.net',
+        port: 3306
+    })
+        .then(conn => {
+            conn.query('use reseptiapi') // Execute a query
+            conn.query('INSERT INTO ainesosat(nimi, reseptiID) VALUES("'+nimi+'", '+reseptiId+')') // Execute a query
+                .then(conn.destroy()) // Close the connection
+        })
+}
+
+mysqlConnectionTest();
+reseptiLista();
+//reseptiHaku('Pasta bolognese');
+//ainesosaHaku(1
+//syotaResepti("Tosca Omenat",55,"Pilko omenat ja laita hiutaleiden kanssa uuniin");
+//syotaAinesosa("Kaurahiutaleet", 5);
+//syotaAinesosa("Omenat",5);
