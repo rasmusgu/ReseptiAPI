652044a9648a82752860d2fe5bbca560276f8c59
Rasmus Gustafsson <rasmus.gustafsson@mail.com>
2019-03-05 17:40:06 +0200

    Restored deleted code

--- mysqlConnect.js
+++ mysqlConnect.js
@@ -1,3 +1,22 @@
+var mariadb = require('mariadb')
+
+mariadb.createConnection({ // Open a new connection
+    user: 'monty',
+    password: 'metrofilia1',
+    host: 'haxers.ddns.net',
+    port: 3306
+})
+    .then(conn => {
+        conn.query('SELECT "Hello world!" as my_message') // Execute a query
+            .then(result => { // Print the results
+                for (row of result) {
+                    console.log(row)
+                }
+            })
+            .then(conn.destroy()) // Close the connection
+    })
+
+
 function reseptiLista() {
     mariadb.createConnection({ // Open a new connection
         user: 'monty',
@@ -12,11 +31,11 @@ function reseptiLista() {
                     for (row of result) {
                         console.log(row)
 
-                })
+                    })
                 .then(conn.destroy()) // Close the connection
             return result
         })
-    }
+}
 }
 
 function reseptiHaku(reseptinNimi) {
@@ -27,7 +46,7 @@ function reseptiHaku(reseptinNimi) {
         port: 3306
     })
         .then(conn => {
-            conn.query('use reseptiapi') // Valitsee tietokannan
+            conn.query('use reseptiapi') // Execute a query
             conn.query('SELECT * FROM reseptit') // Execute a query
                 .then(result => { // Print the results
                     for (row of result) {
@@ -37,5 +56,5 @@ function reseptiHaku(reseptinNimi) {
                 .then(conn.destroy()) // Close the connection
             return result
         })
-        }
+}
 }
