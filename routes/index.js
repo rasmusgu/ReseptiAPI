bc90f3a257bf58ff53cd2047a4de7b44ffb57dcf
Marko Korhonen <reekymarko@reekynet.com>
2019-03-05 16:34:36 +0200

    Switched from pug to html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'ReseptiAPI' });
+});/
+
+/* GET test page. */
+router.get('/rasse', function(req, res, next) {
+  res.sendfile('rasse.html');
 });
 
 module.exports = router;
