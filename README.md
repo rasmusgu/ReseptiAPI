374d682a5163069be22b3257950fd21dc660e643
joritu <jori.tuomiranta@hotmial.com>
2019-03-12 00:43:33 +0200

    vähän dokumentaatiota ja väärien request headerien käsittelyä

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 A simple API to add and read recipes
+
+Sallitut HTTP requestit
+    GET
+    POST
+ 
+ 
+    
+Hae kaikki reseptit
+  GET  /api/lista
+  palauttaa listan kaikista resepteistä
+  Esimerkki responsesta:
+    
+        [
+            {
+                "id": 1,
+                "nimi": "Pasta bolognese\r\n",
+                "valmistusaika": 45,
+                "kokkausohje": "Keitä pastat",
+                "kuva": "https://www.recipetineats.com/wp-content/uploads/2016/08/Spaghetti-Bolognese_3.jpg",
+                "GROUP_CONCAT(a.`nimi` separator ', ')": "Tomaatti: 3"
+            },
+            {
+                "id": 3,
+                "nimi": "Puuro",
+                "valmistusaika": 5,
+                "kokkausohje": "Mikrota 5 minuuttia",
+                "kuva": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/10/10/17/38-porridge-ala.jpg",
+                "GROUP_CONCAT(a.`nimi` separator ', ')": "Kaurahiutaleet: 2 dl"
+            }
+         ]
+    
+    
+
+Hae reseptejä millä tahansa hakutermillä
+    POST /api/haeResepti
+    Content type täytyy olla "application/json" 
+    Jsonin täytyy olla muodossa {"haku": "hakutermi" }
+    esimerkkihaku:
+    
+         {"haku": "puuro" }
+
+Hae reseptejä reseptin id:llä
+   POST /api/haeReseptiById
+   Content type täytyy olla "application/json" 
+   Jsonin täytyy olla muodossa {"id": "idnumero" }
+   
+   esimerkkihaku:
+        
+             {"id: "1" }
+             
+             
+Lisää reseptejä
+             
+/api/lisaaResepti'
--- routes/api.js
+++ routes/api.js
@@ -23,15 +23,14 @@ router.get('/api/lista', function(req, res, next){
 router.post('/api/haeResepti', function(req, res){
     var recipe = req.body.haku;
 
+
     //console.log(req.body.haku);
-
-
-
-
-    mysqlConnect.reseptiHaku(recipe, function(returnvalue){
-      //console.log(returnvalue)
-      res.send(JSON.parse(returnvalue));
-    })
+        if (req.header('Content-type') == "application/json") {                                                  //katso tuleeko requestissa application:json headeri
+            mysqlConnect.reseptiHaku(recipe, function (returnvalue) {
+                //console.log(returnvalue)
+                res.send(JSON.parse(returnvalue));
+            })
+        }else res.sendStatus(406);
 })
 
 
@@ -41,32 +40,31 @@ router.post('/api/lisaaResepti', function(req, res) {
     var kokkausohje= req.body.kokkausohje;
     var kuva = req.body.kuva;
     //console.log(req.body);
-
-    mysqlConnect.syotaResepti(nimi, valmistusaika, kokkausohje, kuva, function (returnvalue) {
+    if (nimi || valmistusaika || kokkausohje || kuva != null  ) {
+         mysqlConnect.syotaResepti(nimi, valmistusaika, kokkausohje, kuva, function (returnvalue) {
 
         //console.log(returnvalue.warningStatus);
 
-        if (returnvalue.warningStatus == 0){
+        if (returnvalue.warningStatus == 0) {
             res.sendStatus(200);
-        }else{
+        } else {
             res.sendStatus(500);
         }
-
-
-    })
+         })
+    }
 })
 
 
 router.post('/api/haeReseptiById', function(req, res, next){
+ //console.log(req.header('Content-type'));
+    if (req.header('Content-type') == "application/json") {
+        var id = req.body.id;
+        mysqlConnect.haeReseptiById(id, function (returnvalue) {
+            console.log(returnvalue)
 
-    var id = req.body.id;
-
-    //console.log(req.body.id);
-
-    mysqlConnect.haeReseptiById(id, function(returnvalue){         //täytyy laittaa reseptilistalle parametrina funktio, minkä se osaa ajaa myöhemmin
-        //console.log(returnvalue)
-        res.send(JSON.parse(returnvalue));
-    })
+            res.send(JSON.parse(returnvalue));
+        })
+    } else res.sendStatus(406);
 
 })
 
